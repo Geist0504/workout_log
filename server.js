19081234c160d15f9b00a900207d8e0205964007
Glitch (earthy-armadillo) <none>
2018-11-19 13:18:36 +0000

    🐬📯 Checkpoint ./server.js:2025103/2347

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ let Workout_Schema = new Schema({
 
 User_Schema.plugin(autoIncrement.plugin, {model: 'User_Schema', field: 'userId'});
 
-let URL_model = connection.model('User', User_Schema)
+let User_model = connection.model('User', User_Schema)
 let Workout_model = connection.model('Workouts', Workout_Schema)
 
 
@@ -58,11 +58,36 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
-app.post("/api/exercise/new-user", function (req, res){})
+app.post("/api/exercise/new-user", function (req, res){
+  let username = req.body.username;
+  let record = new User_model({user: username})
+  try{
+      let result = record.save();
+      res.
+    }
+    catch(err){
+      if (err.name === 'MongoError' && err.code === 11000) {
+        res.status(409).send(new Error('Duplicate key', [err.message]));
+      }
+      res.status(500).send(err);
+    }
+})
 
 app.post("/api/exercise/log", function (req, res){})
 
-app.get("/api/exercise/users", function (req, res){})
+app.get("/api/exercise/users", function (req, res){
+  try{
+    let entries = User_model.find({}, (err, users) => {
+      res.json(users)
+    })
+  }
+  catch(err){
+    if (err.name === 'MongoError' && err.code === 11000) {
+            res.status(409).send(new Error('Duplicate key', [err.message]));
+          }
+          res.status(500).send(err);
+  }
+})
 
 app.get("/api/exercise/users/:log/:from?/:to?/:limit?", function (req, res){})
 
