a5af8dda95809099576b2180bfe8e21ac03546b9
Glitch (earthy-armadillo) <none>
2018-11-19 13:07:41 +0000

    🎎🍷 Checkpoint ./server.js:2025103/932 ./package.json:2025103/1176

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+const mongoose = require('mongoose'),
+      autoIncrement = require('mongoose-auto-increment');
+let connection = mongoose.createConnection(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+autoIncrement.initialize(connection)
 const Schema = mongoose.Schema
 app.use(cors())
 
@@ -19,6 +21,12 @@ let User_Schema = new Schema({
 let Workout_Schema = new Schema({
   user: {type:String, unique:true}
 })
+
+User_Schema.plugin(autoIncrement.plugin, {model: 'User_Schema', field: 'userId'});
+
+let URL_model = connection.model('User', User_Schema)
+let Workout_model = connection.model('Workouts', Workout_Schema)
+
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -49,6 +57,15 @@ app.use((err, req, res, next) => {
   res.status(errCode).type('txt')
     .send(errMessage)
 })
+
+app.post("/api/exercise/new-user", function (req, res){})
+
+app.post("/api/exercise/log", function (req, res){})
+
+app.get("/api/exercise/users", function (req, res){})
+
+app.get("/api/exercise/users/:log/:from?/:to?/:limit?", function (req, res){})
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
